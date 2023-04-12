77f30a59c62264f15f67238269853e0408899533
ThanhHip85 <nguyenhiep0456@gmail.com>
2023-04-12 22:58:36 +0700

    thêm hàm c
    + format code

--- src/main/java/com/test/fork/fork/controller/testController.java
+++ src/main/java/com/test/fork/fork/controller/testController.java
@@ -5,4 +5,7 @@ public class testController {
     public static void main(String[] args) {
         System.out.println("helloo a hieppp dep zzaiii1233333333333");
     }
+    public void test() {
+        System.out.println("test típ theooooo");
+    }
 }
