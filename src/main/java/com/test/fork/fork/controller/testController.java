aa8bd85332231bf48ec782f427c474c804020a31
ThanhHip85 <nguyenhiep0456@gmail.com>
2023-04-12 22:54:03 +0700

    thêm hàm a
    + format code

--- src/main/java/com/test/fork/fork/controller/testController.java
+++ src/main/java/com/test/fork/fork/controller/testController.java
@@ -1,4 +1,8 @@
 package com.test.fork.fork.controller;
 
 public class testController {
+
+    public static void main(String[] args) {
+        System.out.println("helloo a hieppp dep zzaiii1233333333333");
+    }
 }
